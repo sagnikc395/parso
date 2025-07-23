c1b35084a919aacfb233ca668942ebc99e820f50
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-07-23 18:37:11 +0530

    feat: added main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from parso!")
+from parso.types import ParserInput, Position
+from parso.parser_json import json_value
 
+def main():
+    input = ParserInput('{"name": "Alice", "age": 30, "isMember": true}', Position(0, 1, 1))
+    result = json_value().parse(input)
+    print(result)
 
 if __name__ == "__main__":
     main()
